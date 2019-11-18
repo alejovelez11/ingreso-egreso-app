f88ce720a3a53d7370893049314edc89dbc4df82
Alejandro Velez <alejovg0911@gmail.com>
2019-11-18 13:14:53 -0500

    seccion 8 lista

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -5,15 +5,21 @@ import Swal from 'sweetalert2'
 import {map} from "rxjs/operators";
 import { User } from './user.model';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { Store } from '@ngrx/store';
+import { AppState } from '../app.reducer';
+import { ActivarLoadingAction, DesactivarLoadingAction } from '../shared/ui.actions';
+import { SetUserAction } from './auth.actions';
+import { Subscription } from 'rxjs';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-
-  constructor(private afAuth:AngularFireAuth, private router:Router, private afDB:AngularFirestore) { }
+  private subscription:Subscription = new Subscription()
+  constructor(private afAuth:AngularFireAuth, private router:Router, private afDB:AngularFirestore, private store:Store<AppState>) { }
   crearUsuario(nombre:string, email:string, pasword:string){
+    this.store.dispatch(new ActivarLoadingAction())
     this.afAuth.auth.createUserWithEmailAndPassword(email, pasword).then(resp => {
       const user: User = {
         uid: resp.user.uid,
@@ -22,8 +28,11 @@ export class AuthService {
       }
       this.afDB.doc(`${user.uid}/usuario`).set(user).then(()=>{
         this.router.navigate(["/"])
+        this.store.dispatch(new DesactivarLoadingAction())
+        
       })
     }).catch(error => {
+      this.store.dispatch(new DesactivarLoadingAction())
       Swal.fire({
         title: 'Error en el registro',
         text: error.message,
@@ -31,20 +40,31 @@ export class AuthService {
         icon:"error",
         confirmButtonText: 'Aceptar'
       })  
-
+      
       
     })
   }
+  
   initAuthListener(){
-    this.afAuth.authState.subscribe(fbuser=>{
-      console.log(fbuser);
+    this.afAuth.authState.subscribe(fbuser => {
+      if (fbuser) {
+        this.subscription = this.afDB.doc(`${fbuser.uid}/usuario`).valueChanges().subscribe((usuario:any)=>{
+          const newUser = new User(usuario)
+          this.store.dispatch(new SetUserAction(newUser))
+        })
+      } else {
+        this.subscription.unsubscribe()
+      }
       
     })
   }
   login(email:string, password:string){
+    this.store.dispatch(new ActivarLoadingAction())
     this.afAuth.auth.signInWithEmailAndPassword(email,password).then(resp=>{
       this.router.navigate(["/"])
+      this.store.dispatch(new DesactivarLoadingAction())
     }).catch(error=>{
+      this.store.dispatch(new DesactivarLoadingAction())
       Swal.fire({
         title: 'Error en el login',
         text: error.message,
